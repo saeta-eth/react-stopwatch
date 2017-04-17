925f6c57d1e56b2b4f6647831151aed85cd94dba
slorenzo <SebastianLorenzo@gmail.com>
2017-04-17 15:32:48 -0300

    Added base scaffold

---
+++ example/index.js
@@ -0,0 +1,11 @@
+/* @flow */
+import React from 'react'
+import ReactDom from 'react-dom'
+//import Stopwatch from '../../src/index'
+
+ReactDom.render(
+  <div>
+    You opened this page.
+  </div>,
+  document.getElementById('app')
+)
