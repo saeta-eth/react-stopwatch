0ec1cd568721719213d5415669959ff8e118034a
slorenzo <SebastianLorenzo@gmail.com>
2017-04-21 16:59:56 -0300

    Added fillTextWithFormat method inside of utils

---
+++ src/utils.js
@@ -0,0 +1,26 @@
+/* @flow */
+
+const fillTextWithFormat = function fillTextWithFormat(hours, minutes, seconds): string {
+  let finalHours;
+  let finalMinutes;
+  let finalSeconds;
+
+  if (hours > 9) {
+    finalHours = hours;
+  } else {
+    finalHours = `0${hours}`;
+  }
+  if (minutes > 9) {
+    finalMinutes = minutes;
+  } else {
+    finalMinutes = `0${minutes}`;
+  }
+  if (seconds > 9) {
+    finalSeconds = seconds;
+  } else {
+    finalSeconds = `0${seconds}`;
+  }
+  return `${finalHours}:${finalMinutes}:${finalSeconds}`;
+};
+
+export default fillTextWithFormat;
